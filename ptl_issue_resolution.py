ac4b18d21d4db784772d75d122813a8a172f326d
James Folk <jamesfolk1@gmail.com>
2024-02-23 09:22:56 -0500

    added simple tool for getting the text to promote the ptl issue resolver

---
+++ ptl_issue_resolution.py
@@ -0,0 +1,7 @@
+
+message = """
+PTL's; When getting help from our managers, please use this [form](https://docs.google.com/forms/d/e/1FAIpQLSe-0D-8XUg_xJsEg0faOnWfiRzzA_KqgDsXbjMXqJpKEgx7vw/viewform?usp=sf_link)
+Then monitor the form on this [excel sheet](https://docs.google.com/spreadsheets/d/1ZGWAkIcK4vXzaQwfolC5Hg8r3IvVo5UxZmBwzBZeeT8/edit?usp=sharing)
+"""
+
+print(message)
